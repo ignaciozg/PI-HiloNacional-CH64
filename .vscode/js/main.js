8a95ff1a24f806f58bfbe774fc12fdd2d4deb7d7
GabrielaEsparza1 <gerspaguetti1@gmail.com>
2026-02-12 16:20:00 -0600

    Agrego validacion de correo y nombre

--- .vscode/js/main.js
+++ .vscode/js/main.js
@@ -65,6 +65,42 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+//Evitar que la pagina se recargue al hacer click en enviar:
+document.getElementById("formulario").addEventListener("submit", function(event){event.preventDefault();
+//declaramos las variables que van dentro del form(nombre,correo) y que nos regrese el valor que ingreso el usuarios(.vulue), luego quitamos espacios del final y el principio por si acaso(.trim)
+let nombre = document.getElementById("nombre").value.trim();
+let correo = document.getElementById("correo").value.trim();
+//declaramos el mensaje vacio que despues llenaremos
+let mensaje = document.getElementById("mensaje");
+//Ahora vamos declaramos exppresiones regulares para despues declarar si el correo y el nombre si son validos
+let regexNombre = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+let regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//ahora hacemos un if para evitar que dejen campos vacios:
+if(correo === "" || nombre === ""){
+    mensaje.textContent= "Todos los campos son obligatorios"
+    mensaje.style.color = "red"
+    return;
+}
+
+//Ahora validamos el si el nombre NO(!) cumple el patron con el metodo .test() agregaremos un mensaje de error
+if(!regexNombre.test(nombre)){
+    mensaje.textContent = "Formato de nombre no valido";
+    mensaje.style.color = "red";
+    return;
+}
+//Hacemos lo mismo con correo
+if(!regexCorreo.test(correo)){
+    mensaje.textContent = "Formato de correo no valido";
+    mensaje.style.color = "red";
+    return;
+}
+
+mensaje.textContent = "formulario enviado correctamente ";
+mensaje.style.color = "green";});
+
+
+
 
 
 
